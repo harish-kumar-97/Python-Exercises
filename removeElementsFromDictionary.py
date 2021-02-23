5d6df127c7b8725cdb91bb2ef7974843d465f94d
harish-kumar-97 <harishec031@gmail.com>
2021-02-23 20:05:59 +0530

    remove elements from dictionary

--- removeElementsFromDictionary.py
+++ removeElementsFromDictionary.py
@@ -17,3 +17,7 @@ print(Dict)
 pop_ele = Dict.pop(5)
 print('\nDictionary after deletion using pop(): ' + str(Dict))
 print('Value asssociated to popped key is: ' + str(pop_ele))
+
+Dict.clear()
+print("\nDictionary after clearing using clear()")
+print(Dict)
