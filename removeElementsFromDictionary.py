f9531a2b1dd310c1b3d7659b7d22235dfe521e86
harish-kumar-97 <harishec031@gmail.com>
2021-02-23 20:02:52 +0530

    remove elements from dictionary

---
+++ removeElementsFromDictionary.py
@@ -0,0 +1,19 @@
+# initial dictionary
+Dict = {5 : 'Welcome', 6 : 'To', 7 : 'Geeks', 'A' : {1 : 'Geeks', 2 : 'for', 3 : 'Geeks'}, 'B' : {1 : 'Geeks', 2 : 'Life'}}
+print("Initial Dictionary: ")
+print(Dict)
+
+# Deleting a Key value
+del Dict[6]
+print("\nDeleting a specific key: ")
+print(Dict)
+
+# Deleting a Key from nested dictionary
+del Dict['A'][2]
+print("\nDeleting a key from Nested Dictionary: ")
+print(Dict)
+
+# remove elements using pop() method
+pop_ele = Dict.pop(5)
+print('\nDictionary after deletion using pop(): ' + str(Dict))
+print('Value asssociated to popped key is: ' + str(pop_ele))
