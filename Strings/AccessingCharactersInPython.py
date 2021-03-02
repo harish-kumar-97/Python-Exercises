e3ac5a14b07243b41a92add1729ce8d37080457a
harish-kumar-97 <harishec031@gmail.com>
2021-03-02 18:58:54 +0530

    accessing characters in python

---
+++ Strings/AccessingCharactersInPython.py
@@ -0,0 +1,13 @@
+# Python program to access characters of String
+
+String1 = "GeeksForGeeks"
+print("Initial String: ")
+print(String1)
+
+# Printing First Character
+print("\nFirst character of string is: ")
+print(String1[0])
+
+# Printing Last character
+print("\nLast character of String is: ")
+print(String1[-1])
